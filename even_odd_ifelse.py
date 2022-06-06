81a7fb05579d3d9fcb0e76d5a50433ff3c28f023
krishna-735722 <95479037+krishna-735722@users.noreply.github.com>
2022-06-06 19:18:26 +0530

    Create even_odd_ifelse.py

---
+++ even_odd_ifelse.py
@@ -0,0 +1,10 @@
+# even odd 
+
+print("ennter the number you want to check")
+number=int(input("enter the number : "))
+
+if( number%2==0):
+    print("the given no is even")
+else:
+    print("the given no is odd ") 
+       
