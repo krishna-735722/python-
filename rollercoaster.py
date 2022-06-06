a5a9f78ef40967b7e20e0cc2386b07fac84562d1
krishna-735722 <95479037+krishna-735722@users.noreply.github.com>
2022-06-06 19:53:54 +0530

    Update and rename if_else2.py to rollercoaster.py

---
+++ rollercoaster.py
@@ -0,0 +1,12 @@
+print("welcome to rollercoaster")
+height=int(input("what is your height?\n"))
+age =int(input("enter your age : "))
+
+if height>120:
+  print("you can ride the rollercoater")
+  if age>18:
+    print("you want to pay 12$.")
+  else:
+    print("you want to pay 7$.")  
+else:
+  print("you can't ride the rollercoaster")
