2094d06372312e98794c237f9306c8789942ecb2
krishna-735722 <95479037+krishna-735722@users.noreply.github.com>
2022-06-06 18:37:21 +0530

    Create if_else2.py

---
+++ if_else2.py
@@ -0,0 +1,7 @@
+print("welcome to rollercoaster")
+height=int(input("what is your height?\n"))
+
+if height>120:
+  print("you can ride hte rollercoater")
+else:
+  print("you can't ride the rollercoaster")
